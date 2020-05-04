dad022ee46148dd24d5d698f13ea134be5ca02b5
Harith Manoj <harithpub@gmail.com>
2020-05-04 20:24:37 +0530

    Added lines.cpp for defining functions declared in lines.h for clean division between interface and implementation

---
+++ src/lines.cpp
@@ -0,0 +1,22 @@
+//	This file is part of Lines.
+//
+//	Copyright 2020 Harith Manoj <harithpub@gmail.com>
+//
+//	Lines is free software : you can redistribute itand /or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+
+//	Lines is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with Lines.If not, see < https://www.gnu.org/licenses/>.
+
+
+
+//
+//	This File is for the defining functions declared in lines.h.
+//
